9d0acf921d00428098b3509129004b2c44b09b8e
David Sikar (ubuntu) <sikardavid@gmail.com>
2025-05-25 21:13:40 +1200

    updated starter

--- dental_ussd/models.py
+++ dental_ussd/models.py
@@ -2,3 +2,4 @@
 # Models are the backbone of any Django application.
 # They define the structure of your database and how data is stored.
 # In this case, we will create a simple model for a dental appointment.
+    
